80ed9dfb73223aec2d5bff0a2fed635305c18851
SylvainRajkoumar <sylvain.rajkoumar@gmail.com>
2018-11-14 14:47:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     a = DataManager()
     a.loadData()
     b = NeuralNetwork()
-    b.createModel()
+    # b.createModel()
     
     # b.train(a.train_data, a.train_labels, epochs = 100)
     # b.saveModel()
